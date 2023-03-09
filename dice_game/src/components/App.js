bf2549a3e5a44b7b6e4fbc80357214f3a5dfca3a
jhon3242 <rldotnr2@gmail.com>
2023-03-09 16:50:12 +0900

    feat : 프로젝트 생성

---
+++ dice_game/src/components/App.js
@@ -0,0 +1,8 @@
+import Dice from "./Dice"
+function App(){
+    return (
+        <Dice color="black" num={1} />
+    )
+}
+
+export default App;
---
+++ dice_game/src/components/Dice.js
@@ -0,0 +1,26 @@
+import diceWhite01 from "../asset/diceWhite01.svg"
+import diceBlack01 from "../asset/diceBlack01.svg"
+import diceWhite02 from "../asset/diceWhite02.svg"
+import diceBlack02 from "../asset/diceBlack02.svg"
+import diceWhite03 from "../asset/diceWhite03.svg"
+import diceBlack03 from "../asset/diceBlack03.svg"
+import diceWhite04 from "../asset/diceWhite04.svg"
+import diceBlack04 from "../asset/diceBlack04.svg"
+import diceWhite05 from "../asset/diceWhite05.svg"
+import diceBlack05 from "../asset/diceBlack05.svg"
+import diceWhite06 from "../asset/diceWhite06.svg"
+import diceBlack06 from "../asset/diceBlack06.svg"
+
+const DICE_IMAGES = {
+    white : [diceWhite01, diceWhite02, diceWhite03, diceWhite04, diceWhite05, diceWhite06],
+    black : [diceBlack01, diceBlack02, diceBlack03, diceBlack04, diceBlack05, diceBlack06]
+}
+function Dice({color, num}){
+    const src = DICE_IMAGES[color][num - 1];
+    const alt = `${color} ${num};`
+    return (
+        <img src={src} alt={alt}/>
+    )
+}
+
+export default Dice;
