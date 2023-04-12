26eb06f75bf51c18e04eb7817fd4ccb6859b5206
jhon3242 <rldotnr2@gmail.com>
2023-04-12 16:23:01 +0900

    feat(codeThat) : 프로젝트 마무리

---
+++ code_that/src/Main.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './components/App';
+import HomePage from './pages/HomePage';
+import CoursePage from './pages/CoursePage';
+import CourseListPage from './pages/CourseListPage';
+import WishlistPage from './pages/WishlistPage';
+import NotFoundPage from "./pages/NotFoundPage";
+
+function Main() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App/>} >
+            <Route index element={<HomePage/>} />
+            <Route path={"courses"} >
+              <Route index element={<CourseListPage />} />
+              <Route
+                path=":courseSlug"
+                element={<CoursePage />}
+              />
+            </Route>
+            <Route path="wishlist" element={<WishlistPage />} />
+            <Route path="*" element={<NotFoundPage/>} />
+          </Route>
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Main;
